305285298193f7c868f5f9deb80c0be26810060c
find0ub7 <leon.watanabe@gmail.com>
2022-10-09 11:10:59 -0300

    Adds swagger configuration Adds docker compose

--- src/main/java/com/examples/kafka/KafkaController.java
+++ src/main/java/com/examples/kafka/KafkaController.java
@@ -1,12 +1,14 @@
 package com.examples.kafka;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class KafkaController {
--- src/main/java/com/examples/kafka/SwaggerConfiguration.java
+++ src/main/java/com/examples/kafka/SwaggerConfiguration.java
@@ -1,7 +1,31 @@
 package com.examples.kafka;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(
+                        RequestHandlerSelectors.basePackage("com.examples.kafka"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Kafka Examples")
+                .build();
+    }
 }
